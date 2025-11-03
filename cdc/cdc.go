6049f2306393e25b72f6ffa6849256cca4c0c6e2
Rolly Maulana Awangga <awangga@gmail.com>
2025-11-03 20:40:48 +0700

    tambah client connect

---
+++ cdc/cdc.go
@@ -0,0 +1,37 @@
+package cdc
+
+import (
+	"context"
+
+	"github.com/n0z0/cachedb/proto/cachepb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func Connect() (cachepb.CacheClient, *grpc.ClientConn, error) {
+	conn, err := grpc.NewClient("127.0.0.1:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, nil, err
+	}
+	client := cachepb.NewCacheClient(conn)
+	return client, conn, nil
+}
+func Set(key, value string, client cachepb.CacheClient) error {
+	_, err := client.Set(context.Background(), &cachepb.SetRequest{
+		Key:   key,
+		Value: []byte(value),
+	})
+	return err
+}
+func Get(key string, client cachepb.CacheClient) (string, error) {
+	resp, err := client.Get(context.Background(), &cachepb.GetRequest{
+		Key: key,
+	})
+	if err != nil {
+		return "", err
+	}
+	if !resp.Found {
+		return "", nil
+	}
+	return string(resp.Value), nil
+}
